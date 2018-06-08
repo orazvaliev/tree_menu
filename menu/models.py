2392ff650b23cafe85c47c3767452e4410215f74
orazvaliev <ubeautybae@gmail.com>
2018-06-08 17:20:08 +0300

    set_mptt

--- menu/models.py
+++ menu/models.py
@@ -9,35 +9,60 @@ class Menu(models.Model):
     def __str__(self):
         return self.menu_name
 
-    def get_menu_tree(self):
-        objects = Menu.objects.get(menu_name=self.menu_name).items.all()
-        print(objects)
-
 
 class MenuItem(models.Model):
     menu = models.ForeignKey(Menu,
                              blank=False,
-                             related_name='items',
+                             related_name='item',
                              on_delete=models.CASCADE)
-
+    left = models.IntegerField(blank=True,
+                               null=True)
+    right = models.IntegerField(blank=True,
+                                null=True)
     parent = models.ForeignKey('self',
                                blank=True,
                                null=True,
                                related_name='children',
                                on_delete=models.CASCADE)
-
-    name = models.CharField(max_length=25,
-                            unique=True)
-    slug = models.SlugField(primary_key=True,
-                            max_length=30,
+    position = models.IntegerField(blank=True,
+                                   null=True,
+                                   primary_key=True)
+    level = models.IntegerField(blank=True,
+                                null=True)
+    title = models.CharField(max_length=25,
+                             unique=True)
+    slug = models.SlugField(max_length=30,
                             unique=True)
 
     def __str__(self):
-        return self.name
+        level = self.level if self.level else 1
+        i = '| ' if level > 1 else ''
+        return ('|--' * (level - 1)) + i + self.title
+
+    class Meta:
+        ordering = ('left', )
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
+        self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+        self.set_mptt()
 
     def get_absolute_url(self):
         return reverse('menu', kwargs={'slug': self.slug})
+
+    def set_mptt(self, left=1, parent=None, level=1):
+        for i in type(self).objects.filter(parent=parent).order_by('position'):
+            obj = i
+            children_count = 0
+            while obj.children.exists():
+                for child in obj.children.all():
+                    children_count += 1
+                    obj = child
+            data = {
+                'level': level,
+                'left': left,
+                'right': left + (children_count * 2) + 1
+            }
+            type(self).objects.filter(id=i.id).update(**data)
+            left = data['right'] + 1
+            self.set_mptt(left=data['left'] + 1, parent=i.id, level=data['level'] + 1)
